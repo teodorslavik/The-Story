05e3c61403e5cc1fe89b6c1e1deb80a2b83e9940
Teodor <tedy@Teodors-MacBook-Pro.local>
2020-01-11 15:12:49 +0100

    update 7.1.9

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $soubor=fopen("pernikova_chaloupka.db", "r");
 $title = "Příběh";
-mb_internal_encoding(iso-8859-2);
+header("Content-Type: text/html; charset=ISO-8859-2");
 
 echo ("<html>" .
        "<head>" .   
@@ -47,5 +47,5 @@ echo ("</body>" .
        "</html>");
 
 fclose($soubor);
-echo mb_internal_encoding();
+
 ?>
