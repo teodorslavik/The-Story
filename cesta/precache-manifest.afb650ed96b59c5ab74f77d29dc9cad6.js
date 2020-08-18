e89274340291a153c7d4391afe98251d55b55cb6
Teodor <tedy@Teodors-MacBook-Pro.local>
2020-08-18 15:55:46 +0200

    7.8.6. cest 1.2.1

---
+++ cesta/precache-manifest.afb650ed96b59c5ab74f77d29dc9cad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93aff36401b8e5dbd30fca7e7361897f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40dc35cda677e866e726",
+    "url": "/static/css/main.f73012be.chunk.css"
+  },
+  {
+    "revision": "d62c2a8a8635058fadba",
+    "url": "/static/js/2.54ce614a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.54ce614a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40dc35cda677e866e726",
+    "url": "/static/js/main.194007e7.chunk.js"
+  },
+  {
+    "revision": "dec26090cf87837f80e5",
+    "url": "/static/js/runtime-main.e95b8414.js"
+  }
+]);
