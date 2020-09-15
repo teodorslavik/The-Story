59c832837d02105f8d6d1363e7a627c9159732b3
Teodor <tedy@Teodors-MacBook-Pro.local>
2020-09-15 15:44:02 +0200

    test

---
+++ game2/precache-manifest.35faf53ac3a8c460d6057cdbee2fa622.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa1407b3bfe69040c06026460c82654",
+    "url": "/index.html"
+  },
+  {
+    "revision": "996b1909767968127667",
+    "url": "/static/css/main.748c0dac.chunk.css"
+  },
+  {
+    "revision": "1c5dd1c22db0ef5230a4",
+    "url": "/static/js/2.f3f86247.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f3f86247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "996b1909767968127667",
+    "url": "/static/js/main.74ad0ac2.chunk.js"
+  },
+  {
+    "revision": "dec26090cf87837f80e5",
+    "url": "/static/js/runtime-main.e95b8414.js"
+  }
+]);
